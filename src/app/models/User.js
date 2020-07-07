dfca887909318ef5a65c4907d70a0f5ccbe3eb38
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-07-06 23:19:17 -0300

    Criptografia de senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,11 +1,26 @@
+const bcrypt = require("bcryptjs");
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    avatar: DataTypes.STRING,
-    password_hash: DataTypes.STRING,
-    provider: DataTypes.BOOLEAN, // Informação se é um prestador de serviços ou não
-  });
+  const User = sequelize.define(
+    "User",
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING,
+      provider: DataTypes.BOOLEAN, // Informação se é um prestador de serviços ou não
+    },
+    {
+      hooks: {
+        beforeSave: async (user) => {
+          if (user.password) {
+            user.password_hash = await bcrypt.hash(user.password, 8);
+          }
+        },
+      },
+    }
+  );
 
   return User;
 };
