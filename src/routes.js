96aa402dacf955a886247510663a543dc2a54a09
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-07-08 22:08:07 -0300

    Middleware de auth e redirecionando se o usuario estiver authenticado

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,14 @@ const multerConfig = require("./config/multer");
 const upload = require("multer")(multerConfig);
 
 const routes = express.Router();
+const authMiddleware = require("./app/middlewares/auth");
+const guestMiddleware = require("./app/middlewares/guest");
 
 const UserController = require("./app/controllers/UserController");
 const SessionController = require("./app/controllers/SessionController");
+
+// Enable on all routes that start with /app
+routes.use("/app", authMiddleware);
 
 routes.get("/", SessionController.create);
 routes.post("/signin", SessionController.store);
